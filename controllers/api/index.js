7044f24b78a48b3ba910795512385f90ea09a52e
ampatte <ampatterson1@hotmail.com>
2023-04-15 17:28:54 -0400

    made updates to set up connection

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
-const commentRoutes = require('./commentRoutes');
+// const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
-router.use('/comment', commentRoutes);
+// router.use('/comment', commentRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers')
+
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3307;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -27,7 +31,6 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -37,6 +40,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log('Now listening on ${PORT}!');
